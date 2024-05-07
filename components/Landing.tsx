68717bba5fee05401b52c83855ee05c9ed45d2a2
prasanthshan369 <90073917+prasanthshan369@users.noreply.github.com>
2024-05-07 22:35:08 +0530

    Landing.tsx

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import men from '../public/assets/images/men.jpg'
 import Image from 'next/image'
-
+import Link from 'next/link'
+import { MdClose } from "react-icons/md";
 const Landing = () => {
+  const [isOpen, setIsOpen] = useState(true)
   return (
+    <div>
     <div className='md:px-20 px-5 pt-5 shadow pb-1 sm:px-0 bg-white'>
       <div className='flex md:flex-row flex-col justify-around items-center h-full px-5'>
         <div className='flex items-center flex-col justify-center py-10'>
@@ -26,13 +29,28 @@ const Landing = () => {
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-10 h-10c text-blue-600">
               <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm14.024-.983a1.125 1.125 0 0 1 0 1.966l-5.603 3.113A1.125 1.125 0 0 1 9 15.113V8.887c0-.857.921-1.4 1.671-.983l5.603 3.113Z" clip-rule="evenodd" />
             </svg>
-            <a href="#" className='text-blue-900 border-b-2 mx -1 border-blue-900 '>Watch The Video</a>
+            <p onClick={()=>setIsOpen(true)} className='cursor-pointer text-blue-900 border-b-2 mx -1 border-blue-900 '>Watch The Video</p>
           </div>
         </div>
         <div>
           <Image src={men} className='md:h-[493px] h-full mt-10 md:mt-0  md:w-[350px] w-full   ' alt="" />
         </div>
       </div>
+    </div>
+    {
+        isOpen&&(
+          <div className='absolute top-0 w-full h-full  flex justify-center items-center'>
+                   <div className="relative bg-white rounded-lg shadow border-gray-200 border-2 ">
+                   <MdClose onClick={()=>setIsOpen(!isOpen)} className='absolute text-red-600 right-2 top-1  cursor-pointer text-3xl'/>
+            <div className="flex items-center justify-between px-2 pt-10 py-5 md:px-5  border-b rounded-t ">
+      <iframe  className='rounded-md w-[370px] h-[250px] md:w-[500px] md:h-[350px]' src="https://www.youtube.com/embed/CwsjN7fBmc4?si=tu0s000DboLtVmrS" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+            </div>
+            </div>
+
+          </div>
+        )
+      }
     </div>
   )
 }
