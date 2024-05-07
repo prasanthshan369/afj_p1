6d5cffae3f3c8867c76cf078bf24c32902ed256e
prasanthshan369 <90073917+prasanthshan369@users.noreply.github.com>
2024-05-07 22:52:08 +0530

    Landing.tsx

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { MdClose } from "react-icons/md";
 const Landing = () => {
-  const [isOpen, setIsOpen] = useState(true)
+  const [isOpen, setIsOpen] = useState(false)
   return (
     <div>
     <div className='md:px-20 px-5 pt-5 shadow pb-1 sm:px-0 bg-white'>
